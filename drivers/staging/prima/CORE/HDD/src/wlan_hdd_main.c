191b967aeef6b06554e41f41325e63e0dca671c4
Zer0nite <59056771+Zer0nite@users.noreply.github.com>
2020-01-03 20:03:11 +0100

    removed panic

--- drivers/staging/prima/CORE/HDD/src/wlan_hdd_main.c
+++ drivers/staging/prima/CORE/HDD/src/wlan_hdd_main.c
@@ -12399,11 +12399,6 @@ void hdd_wlan_exit(hdd_context_t *pHddCtx)
    hddLog(LOG1, FL("Unregister IPv4 notifier"));
    unregister_inetaddr_notifier(&pHddCtx->ipv4_notifier);
 
-   // Unregister the Net Device Notifier
-   //unregister_netdevice_notifier(&hdd_netdev_notifier);
-   
-   //hdd_stop_all_adapters( pHddCtx );
-
 #ifdef WLAN_BTAMP_FEATURE
    vosStatus = WLANBAP_Stop(pVosContext);
    if (!VOS_IS_STATUS_SUCCESS(vosStatus))
@@ -14213,21 +14208,11 @@ int hdd_wlan_startup(struct device *dev )
       hddLog(VOS_TRACE_LEVEL_FATAL,"%s: hddDevTmRegisterNotifyCallback failed",__func__);
       goto err_unregister_pmops;
    }
-/*
-   // register net device notifier for device change notification
-   ret = register_netdevice_notifier(&hdd_netdev_notifier);
 
-   if(ret < 0)
-   {
-      hddLog(VOS_TRACE_LEVEL_ERROR,"%s: register_netdevice_notifier failed",__func__);
-      goto err_unregister_pmops;
-   }
-*/
    //Initialize the BTC service
    if(btc_activate_service(pHddCtx) != 0)
    {
       hddLog(VOS_TRACE_LEVEL_FATAL,"%s: btc_activate_service failed",__func__);
-      //goto err_reg_netdev;
 	goto err_nl_srv;
    }
 
@@ -14447,9 +14432,6 @@ err_oem_activate_service:
 
 err_btc_activate_service:
    btc_deactivate_service();
-
-//err_reg_netdev:
-   //unregister_netdevice_notifier(&hdd_netdev_notifier);
 
 err_unregister_pmops:
    hddDevTmUnregisterNotifyCallback(pHddCtx);
